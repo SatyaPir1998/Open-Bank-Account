b7cccce8ff1c63aeb7905a03955a35855d32d63d
Satyapir Ghosh <137476888+SatyaPir1998@users.noreply.github.com>
2025-08-07 11:02:52 +0530

    Add data model: User

---
+++ core/data-model/User.js
@@ -0,0 +1,35 @@
+module.exports = {
+  id: "User",
+  label: "User",
+  description: "",
+  fields: [
+  {
+    "id": "1",
+    "name": "firstName",
+    "type": "string",
+    "required": true,
+    "description": "First name of the user"
+  },
+  {
+    "id": "2",
+    "name": "lastName",
+    "type": "string",
+    "required": true,
+    "description": "Last name of the user"
+  },
+  {
+    "id": "3",
+    "name": "email",
+    "type": "string",
+    "required": true,
+    "description": "Email address of the user"
+  },
+  {
+    "id": "4",
+    "name": "phoneNumber",
+    "type": "string",
+    "required": true,
+    "description": "Phone number of the user"
+  }
+]
+};
